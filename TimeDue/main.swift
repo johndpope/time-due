de5d1c02970c889055951d14088e3e008e7de7fc
Vikas <vksmaini@gmail.com>
2014-08-22 15:26:35 +0530

    Initial Commit

---
+++ TimeDue/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  TimeDue
+//
+//  Created by Vikas on 22/08/14.
+//  Copyright (c) 2014 LeftShift. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
