ed702e755ee2d294f5d83b9573b621dadb6c5d70
Vikas <vksmaini@gmail.com>
2014-08-22 19:06:58 +0530

    added AFnetworking submodule mac edition

---
+++ TimeDue/Classes/Other/TimeDue-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  TimeDue-Bridging-Header.h
+//  TimeDue
+//
+//  Created by Vikas on 22/08/14.
+//  Copyright (c) 2014 LeftShift. All rights reserved.
+//
+
+#ifndef TimeDue_TimeDue_Bridging_Header_h
+#define TimeDue_TimeDue_Bridging_Header_h
+
+#import "AFNetworking.h"
+
+#endif
